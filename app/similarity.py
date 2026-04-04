e224c255d21f227804fb622194aa5c4fc77d5ea8
Ky nash <kynash1@outlook.com>
2026-04-04 16:16:07 +0100

    Create similarity.py file for cosine similarity function

---
+++ app/similarity.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def cosine_similarity(v1: np.ndarray, v2: np.ndarray) -> float:
+    if np.linalg.norm(v1) == 0 or np.linalg.norm(v2) == 0:
+        return 0.0
+    return float(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
